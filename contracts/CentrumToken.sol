29bf27ac52c1fb36f6b5ac67015c822048871397
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-07-21 16:48:45 +0100

    Added token max supply and Initial supply

--- contracts/CentrumToken.sol
+++ contracts/CentrumToken.sol
@@ -1,14 +1,19 @@
-// contracts/GLDToken.sol
-// SPDX-License-Identifier: MIT
+// contracts/CentrumToken.sol
+// SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.21;
 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract CentrumToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Centrum", "CTT") {
-        _mint(msg.sender, initialSupply);
+contract CentrumToken is ERC20Capped {
+    address payable public owner;
+
+    constructor(uint256 cap) ERC20("Centrum", "CTT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = msg.sender;
+        _mint(owner, 70000000 * (10 ** decimals()));
     }
 }
