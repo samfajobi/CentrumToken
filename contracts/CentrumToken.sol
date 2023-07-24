277035a207d15e20f8dc4d6380fd672c4e711d96
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-07-24 13:52:56 +0100

    Successfully compiled Code

--- contracts/CentrumToken.sol
+++ contracts/CentrumToken.sol
@@ -1,11 +1,11 @@
 // contracts/CentrumToken.sol
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 
-import "openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
@@ -14,20 +14,26 @@ contract CentrumToken is ERC20Capped, ERC20Burnable {
     
     uint256 public blockReward;
 
-    constructor(uint256 cap, reward ) ERC20("Centrum", "CTT") ERC20Capped(cap * (10 ** decimals())) {
-        owner = msg.sender;
+    constructor(uint256 cap, uint256 reward ) ERC20("Centrum", "CTT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
     
-    function _mintMinerRewardFunction() internal {
+    function _mintMinerReward() internal {
         _mint(block.coinbase, blockReward);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 value) {
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
         if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
-            _mintMinerRewardFunction();
+            _mintMinerReward();
         }
+        super._beforeTokenTransfer(from, to, value);
     }
 
     function setBlockReward(uint256 reward) public onlyOwner {
@@ -35,7 +41,7 @@ contract CentrumToken is ERC20Capped, ERC20Burnable {
     }
     
     function destroy() public onlyOwner {
-        selfdestruct();
+        selfdestruct(owner);
     } 
 
     modifier onlyOwner {
