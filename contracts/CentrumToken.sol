d88391f6d92941607f49114013e5386532acb428
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-07-31 16:54:34 +0100

    Writing tests

--- contracts/CentrumToken.sol
+++ contracts/CentrumToken.sol
@@ -13,37 +13,37 @@ contract CentrumToken is ERC20Capped, ERC20Burnable {
     address payable public owner;
     
     uint256 public blockReward;
-
+    
     constructor(uint256 cap, uint256 reward ) ERC20("Centrum", "CTT") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
-
+    // function overriding the mint function in the base contact
     function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
         require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
         super._mint(account, amount);
     }
-    
+    // minting function
     function _mintMinerReward() internal {
         _mint(block.coinbase, blockReward);
     }
-
+    // smart contract hook to run before minting
     function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
         if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
             _mintMinerReward();
         }
         super._beforeTokenTransfer(from, to, value);
     }
-
+    // reset block_reward function
     function setBlockReward(uint256 reward) public onlyOwner {
         blockReward = reward * (10 ** decimals());
     }
-    
+    // destroy contract function
     function destroy() public onlyOwner {
         selfdestruct(owner);
     } 
-
+    // modifier function
     modifier onlyOwner {
         require(msg.sender == owner, "Only owner can call this function.");
         _;
